b46b3d1a00a8bb53b3b692cbb00dbea0fd94d631
Michal Szczepanski <michal@vane.pl>
2020-02-29 00:13:57 +0100

    Modify device view add list of chromecast names

--- frontend/src/view/device.jsx
+++ frontend/src/view/device.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import chromecastDeviceAll from '../service/device';
 import deviceStore from '../store/deviceStore';
 
 const ChromecastDeviceComponent = () => {
+  const [chromecastList, setChromecastList] = useState(deviceStore.chromecast);
+  const handleChromecastChange = () => {
+    setChromecastList(deviceStore.chromecast);
+  };
+
+  deviceStore.setCallback('chromecast', handleChromecastChange);
   chromecastDeviceAll(deviceStore);
+  const device = [];
+  chromecastList.forEach((el) => {
+    device.push(<p key={el.uuid}>{el.name}</p>);
+  });
   return (
     <div>
       <h1>Device</h1>
+      <div>
+        {device}
+      </div>
     </div>
   );
 };
