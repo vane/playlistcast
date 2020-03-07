b6dd6734c01991288cf8288bbd7e5b5adaf5a82c
Michal Szczepanski <michal@vane.pl>
2020-03-07 16:23:26 +0100

    Update movieTimeFormat remove duplicate second formatting

--- frontend/src/formatter/movieTimeFormat.js
+++ frontend/src/formatter/movieTimeFormat.js
@@ -6,16 +6,15 @@ const movieTimeFormat = (t) => {
   }
   // show minutes:seconds
   const seconds = t % 60;
+  out = seconds < 10 ? `0${seconds}` : seconds;
   let minutes = Math.floor(t / 60);
   if (minutes < 60) {
-    out = seconds < 10 ? `0${seconds}` : seconds;
     out = `${minutes < 10 ? `0${minutes}` : minutes}:${out}`;
     return out;
   }
   // show hours:minutes:seconds
   minutes %= 60;
   const hours = Math.floor(t / 3600);
-  out = seconds < 10 ? `0${seconds}` : seconds;
   out = `${minutes < 10 ? `0${minutes}` : minutes}:${out}`;
   out = `${hours < 10 ? `0${hours}` : hours}:${out}`;
   return out;
