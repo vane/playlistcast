ef918aff697d32731f2e098848708ce77fbae786
Michal Szczepanski <michal@vane.pl>
2020-03-13 21:47:47 +0100

    Add seek backward, forward buttons

--- frontend/src/view/player/chromecast.jsx
+++ frontend/src/view/player/chromecast.jsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { Button, Card } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlayCircle, faPauseCircle } from '@fortawesome/free-regular-svg-icons';
-import { faVolumeUp } from '@fortawesome/free-solid-svg-icons';
+import { faRedo, faUndo, faVolumeUp } from '@fortawesome/free-solid-svg-icons';
 import chromecastStore from 'store/chromecastStore';
-import { chromecastPause, chromecastPlay } from 'service/chromecast';
+import { chromecastPause, chromecastPlay, chromecastSeek } from 'service/chromecast';
 import TimeDisplay from 'view/player/timeDisplay';
 import VolumeComponent from 'view/player/volume';
 import TimeProgress from 'view/player/timeProgress';
+
 
 const ChromecastDeviceComponent = () => {
   const [chromecastList, setChromecastList] = useState(chromecastStore.chromecast);
@@ -55,13 +56,33 @@ const ChromecastDevice = ({ device }) => {
   const handleVolumeChange = () => {
     setVolumeVisible(!volumeVisible);
   };
+  console.log(status.currentTime);
+  const hanleSeekForward = () => {
+    chromecastSeek(device.uuid, status.currentTime + 30);
+  };
+
+  const handleSeekBackward = () => {
+    chromecastSeek(device.uuid, status.currentTime - 30);
+  };
 
   let playPauseButton = null;
 
   if (status.playerState === 'PLAYING') {
-    playPauseButton = <Button type="link" icon={<FontAwesomeIcon icon={faPauseCircle} onClick={handlePauseClick} size="2x" />} />;
+    playPauseButton = (
+      <Button
+        type="link"
+        style={{ marginRight: '10px' }}
+        icon={<FontAwesomeIcon icon={faPauseCircle} onClick={handlePauseClick} size="2x" />}
+      />
+    );
   } else if (status.playerState === 'PAUSED') {
-    playPauseButton = <Button type="link" icon={<FontAwesomeIcon icon={faPlayCircle} onClick={handlePlayClick} size="2x" />} />;
+    playPauseButton = (
+      <Button
+        type="link"
+        style={{ marginRight: '10px' }}
+        icon={<FontAwesomeIcon icon={faPlayCircle} onClick={handlePlayClick} size="2x" />}
+      />
+    );
   }
 
   let name = '';
@@ -89,9 +110,19 @@ const ChromecastDevice = ({ device }) => {
           <Button
             type="link"
             style={{ marginRight: '10px' }}
+            icon={<FontAwesomeIcon icon={faUndo} onClick={handleSeekBackward} size="2x" />}
+          />
+          <Button
+            type="link"
+            style={{ marginRight: '10px' }}
             icon={<FontAwesomeIcon icon={faVolumeUp} onClick={handleVolumeChange} size="2x" />}
           />
           {playPauseButton}
+          <Button
+            type="link"
+            style={{ marginRight: '10px' }}
+            icon={<FontAwesomeIcon icon={faRedo} onClick={hanleSeekForward} size="2x" />}
+          />
         </div>
         {volumeComponent}
       </div>
